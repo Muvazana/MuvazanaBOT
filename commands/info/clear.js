4fafb2d3b06358c79849aae35a86fac73c18337f
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-02 11:57:12 +0700

    update

--- commands/info/clear.js
+++ commands/info/clear.js
@@ -3,7 +3,7 @@ module.exports = {
     category: "info",
     description: "Clear message as many args",
     run: async (client, message, args) =>{
-        if(!args[1]) return message.reply('ERROR Args Please defind second Args');
+        if(!args[1]) return message.reply(args[1]+" <= s");
         if(message.author.id != 543738538932305930) return message.reply('Sorry you cant use this command');
         message.channel.bulkDelete(args[1]);
     }
