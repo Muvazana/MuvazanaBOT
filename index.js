fffee5d56e6d92e8943f4f44cdebe1e070982fc5
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-02 11:48:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -49,42 +49,4 @@ client.on('message', async message =>{
         command.run(client, message, args);
 });
 
-// client.on('message', message =>{
-//     const prefix = '>';
-//     let args = message.content.substring(prefix.length).split(" ");
-//     switch(args[0]){
-//         case 'userinfo':
-            // let person = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[1]));
-            // if(!person) return message.reply("Couldn't find member (please fill next args with mention)");
-            // let infoembed = new Discord.MessageEmbed()
-            //     .setColor(person.displayHexColor)
-            //     .setTitle("Info : ")
-            //     .setAuthor(person.user.username)
-            //     .addField("Full Username ", `${person.user.username}#${person.user.discriminator}`)
-            //     .addField("ID ", person.user.id)
-            //     .setThumbnail(person.user.displayAvatarURL())
-            //     .setTimestamp();
-            // message.channel.send(infoembed);
-//             break;
-//         case 'clear' :
-//             if(!args[1]) return message.reply('ERROR Args Please defind second Args');
-//             if(message.author.id != 543738538932305930) return message.reply('Sorry you cant use this command');
-//             message.channel.bulkDelete(args[1]);
-//             break;
-//         case 'server' :
-//             setInterval({
-                
-//             },);
-//             break;
-//         case 'help' :
-//             let helpembed = new Discord.MessageEmbed()
-//                 .setColor(person.displayHexColor)
-//                 .setTitle("Help : ")
-//                 .addField("Command ", ">userinfo @mention => Get User Info\n>help \t=> Show all command BOT")
-//                 .setTimestamp();
-//             message.channel.send(helpembed);
-//             break;
-//     }
-// })
-
 client.login(process.env.TOKEN);
