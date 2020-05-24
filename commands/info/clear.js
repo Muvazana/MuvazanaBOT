f61fc052741c9cc897e13b0ed02b6a82accc5d42
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-24 20:01:02 +0700

    Update clear.js

--- commands/info/clear.js
+++ commands/info/clear.js
@@ -4,21 +4,21 @@ module.exports = {
     description: "Clear message as many args",
     run: async (client, message, args) =>{
         if(!args[0]) return message.reply('ERROR Args Please defind second Args');
-        if(args[0] != "on" && args[1] != "off"){
+        if(args[0] != "on" && args[0] != "off"){
             if(message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR)){
                 message.channel.bulkDelete(args[0]);
             }else return message.reply('Sorry you cant use this command');
         }
-        if(args[0] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
-            process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR;
-            console.log("ACCESSCLEAR => ", process.env.ACCESSCLEAR);
-            return message.reply('Access Clear for admin has been update to ON');
-        }else if(args[0] == off && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == true){
-            process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR;
-            console.log("ACCESSCLEAR => ", process.env.ACCESSCLEAR);
-            return message.reply('Access Clear for admin has been update to OFF');
-        }else{
-            return message.reply('Sorry you cant use this command, Update FAIL');
-        }
+        // if(args[0] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
+        //     process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR;
+        //     console.log("ACCESSCLEAR => ", process.env.ACCESSCLEAR);
+        //     return message.reply('Access Clear for admin has been update to ON');
+        // }else if(args[0] == off && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == true){
+        //     process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR;
+        //     console.log("ACCESSCLEAR => ", process.env.ACCESSCLEAR);
+        //     return message.reply('Access Clear for admin has been update to OFF');
+        // }else{
+        //     return message.reply('Sorry you cant use this command, Update FAIL');
+        // }
     }
 }
