eca47cb91174bf86a7d0366250d485d19c06b591
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-24 19:56:56 +0700

    Update clear.js

--- commands/info/clear.js
+++ commands/info/clear.js
@@ -4,14 +4,15 @@ module.exports = {
     description: "Clear message as many args",
     run: async (client, message, args) =>{
         if(!args[0]) return message.reply('ERROR Args Please defind second Args');
-
-        if((message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR) && args[1] != "on" && args[1] != "off")){
-            message.channel.bulkDelete(args[0]);
-        }else return message.reply('Sorry you cant use this command');
-        if(args[1] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
+        if(args[0] != "on" && args[1] != "off"){
+            if(message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR)){
+                message.channel.bulkDelete(args[0]);
+            }else return message.reply('Sorry you cant use this command');
+        }
+        if(args[0] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
             process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR
             return message.reply('Access Clear for admin has been update to ON');
-        }else if(args[1] == off && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == true){
+        }else if(args[0] == off && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == true){
             process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR
             return message.reply('Access Clear for admin has been update to OFF');
         }else{
