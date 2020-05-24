93dccdbca2a4ffb559c84b5d42d4bb2149f90cf4
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-24 19:48:17 +0700

    update clear

--- commands/info/clear.js
+++ commands/info/clear.js
@@ -4,7 +4,18 @@ module.exports = {
     description: "Clear message as many args",
     run: async (client, message, args) =>{
         if(!args[0]) return message.reply('ERROR Args Please defind second Args');
-        if(message.author.id != 543738538932305930) return message.reply('Sorry you cant use this command');
-        message.channel.bulkDelete(args[0]);
+
+        if(message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR)){
+            message.channel.bulkDelete(args[0]);
+        }else return message.reply('Sorry you cant use this command');
+        if(args[1] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
+            process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR
+            return message.reply('Access Clear for admin has been update to ON');
+        }else if(args[1] == off && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == true){
+            process.env.ACCESSCLEAR = !process.env.ACCESSCLEAR
+            return message.reply('Access Clear for admin has been update to OFF');
+        }else{
+            return message.reply('Sorry you cant use this command, Update FAIL');
+        }
     }
 }
