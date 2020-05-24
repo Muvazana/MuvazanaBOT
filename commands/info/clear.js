3ff390853155188cba8cf6ead35d15031e436089
dravity0 <30398479+dravity0@users.noreply.github.com>
2020-05-24 19:54:17 +0700

    Update clear.js

--- commands/info/clear.js
+++ commands/info/clear.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (client, message, args) =>{
         if(!args[0]) return message.reply('ERROR Args Please defind second Args');
 
-        if(message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR)){
+        if((message.author.id == 543738538932305930 || (message.member.roles.cache.find("name", "admin") && process.env.ACCESSCLEAR) && args[1] != "on" && args[1] != "off")){
             message.channel.bulkDelete(args[0]);
         }else return message.reply('Sorry you cant use this command');
         if(args[1] == on && message.author.id == 543738538932305930 && process.env.ACCESSCLEAR == false){
